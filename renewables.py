67b00f58af913d67718cbbc71a42072ed747a2b8
Johannes Staffans <johannes.staffans@gmail.com>
2017-12-15 23:04:39 +0100

    Test API

--- renewables.py
+++ renewables.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from pyiso import client_factory
+import click
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.cli.command()
+@click.argument('control_area')
+def generation(control_area):
+    entso = client_factory('EU')
+    generation = entso.get_generation(latest=True, control_area=control_area)
+
+    # import json
+    # print(json.dumps(generation, indent=2, default=str))
