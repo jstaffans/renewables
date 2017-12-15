04ad69a30d39faf4a2a424f36e425dc87a0f99e8
Johannes Staffans <johannes.staffans@gmail.com>
2017-12-15 23:19:53 +0100

    date option

--- renewables.py
+++ renewables.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from pyiso import client_factory
 import click
+rom dateutil.parser import parse
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 
+def parse_date(d):
+    try:
+        date = parse(d)
+    except:
+        raise click.BadParameter("Couldn't parse date.", param=d)
+    return date
 
 @app.cli.command()
+@click.option('--date', default='{:%Y-%m-%d}'.format(datetime.now() - timedelta(days=1)))
 @click.argument('control_area')
-def generation(control_area):
+def generation(date, control_area):
     entso = client_factory('EU')
-    generation = entso.get_generation(latest=True, control_area=control_area)
+    start = parse_date(date)
+    end = start + timedelta(days=1)
+    generation = entso.get_generation(latest=True, control_area=control_area, start_at=start, end_at=end)
 
-    # import json
-    # print(json.dumps(generation, indent=2, default=str))
+    import json
+    print(json.dumps(generation, indent=2, default=str))
