e1a65608b4eb030a6b789cedba39039febfcc8d6
Johannes Staffans <johannes.staffans@gmail.com>
2018-02-10 22:51:35 +0100

    chart styling

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,26 +6,23 @@ import * as d3 from 'd3';
   const width = +chart.attr('width');
   const height = +chart.attr('height');
 
-  debugger;
-  const x = d3.scaleLinear().domain([0, global.generationData.length]).range([40, width - 40]);
+  const x = d3.scaleLinear().domain([0, global.generationData.length]).range([30, width - 30]);
   const y = d3.scaleLinear().domain([0, 1]).range([height, 0]);
-  const line = d3.line().x((d, i) => {
-    console.log(`Plotting ${i} to be at ${x(i)}`);
-    return x(i);
-  }).y(d => y(d));
 
-  // const xAxis = d3.axisBottom(x);
-  // chart.append('svg:g')
-  //   .attr('class', 'axis--x')
-  //   .attr('transform', `translate(0, ${height})`)
-  //   .call(xAxis);
+  const observed = d3.line().x((d, i) => x(i)).y(d => y(d)).curve(d3.curveLinear);
+  const predicted = d3.line().x((d, i) => x(i + 23) + 5).y(d => y(d)).curve(d3.curveLinear);
 
   const yAxis = d3.axisLeft(y);
   chart.append('svg:g')
-    .attr('class', 'axis--y')
+    .attr('class', 'axis axis--y')
     .call(yAxis);
 
-  chart.append('svg:path').attr('d', line(global.generationData));
+  const obs = global.generationData.slice(0, 24);
+  const pred = global.generationData.slice(24);
+  pred.unshift(obs[obs.length - 1]);
+
+  chart.append('svg:path').attr('class', 'line').attr('d', observed(obs));
+  chart.append('svg:path').attr('class', 'line line--predicted').attr('d', predicted(pred));
 
 })(window);
 
