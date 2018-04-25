35e6a7531cf1a7da1484573c37f352affaf2b02f
Johannes Staffans <johannes.staffans@gmail.com>
2018-04-25 22:24:43 +0200

    use sunrise, sunset from Astral

--- app/tasks/sun.py
+++ app/tasks/sun.py
@@ -12,16 +12,25 @@ def _hour_range(day):
         yield hour
         hour = hour + timedelta(hours=1)
 
+def _is_sun_up(sunrise, sunset, t):
+    t_with_tz = t.replace(tzinfo=sunrise.tzinfo)
+    return 1 if sunrise < t_with_tz < sunset else 0
+
 def sun_calendar(city_name, start, end):
     """
     Returns an hourly sun calendar for the given city. The calendar contains the
     information whether or not the sun is up during a given day and hour.
     """
 
-    records = []
+    a = Astral()
+    city = a[city_name]
+    # d = start.replace(tzinfo=city.tz)
     d = start
+    records = []
     while d < end:
-        records += [[t] for t in _hour_range(d)]
+        sunrise = city.sunrise(date=d)
+        sunset = city.sunset(date=d)
+        records += [[t, _is_sun_up(sunrise, sunset, t)] for t in _hour_range(d)]
         d = d + timedelta(days=1)
 
-    return pd.DataFrame.from_records(records, columns=['timestamp'])
+    return pd.DataFrame.from_records(records, columns=['timestamp', 'sun'])
--- tests/tasks/test_sun.py
+++ tests/tasks/test_sun.py
@@ -26,3 +26,9 @@ class TestSun(object):
         assert rows == 48
         assert calendar['timestamp'][0] == datetime(2018, 4, 25)
         assert calendar['timestamp'][47] == datetime(2018, 4, 26, 23)
+
+    def test_night(self):
+        start = datetime(2018, 4, 25, 21, 53, 0)
+        calendar = sun.sun_calendar('Berlin', start, start + timedelta(days=1))
+        assert calendar['sun'][0] == 0
+        assert calendar['sun'][11] == 1
