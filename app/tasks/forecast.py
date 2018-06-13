ee494b1c9ab0dfa8812f0a17068fd26224d79ec7
Johannes Staffans <johannes.staffans@gmail.com>
2018-06-13 14:45:57 +0200

    Forecast preparation: fetch latest weather forecast

--- app/tasks/forecast.py
+++ app/tasks/forecast.py
@@ -25,3 +25,15 @@ def check_historical_data_present(hour):
     return (
         len(generation_reports) >= HOURS_PAST and len(weather_forecasts) >= HOURS_PAST
     )
+
+
+def prepare_forecast(weather_task, api_token, city_name, hour):
+    """
+    Prepare database for calculating a forecast.
+    """
+    forecast_horizon = hour + timedelta(hours=WEATHER_FORECAST_HOURS_FUTURE)
+
+    weather_forecast = weather_task(api_token, city_name, hour, forecast_horizon)
+    weather_forecast_window = weather_forecast.ix[hour:forecast_horizon]
+
+    WeatherForecast.insert(city_name, weather_forecast_window)
--- tests/tasks/test_forecast.py
+++ tests/tasks/test_forecast.py
@@ -6,11 +6,14 @@ from app import create_app, db
 from app.model import GenerationReport, WeatherForecast
 from app.tasks.forecast import (
     check_historical_data_present,
+    prepare_forecast,
+    WEATHER_FORECAST_HOURS_FUTURE,
 )
 from app.util import hour_now
 from tests.df_helper import (
     timestamped_single_generation_report,
     timestamped_single_weather_forecast,
+    current_weather_forecast,
 )
 
 
@@ -41,6 +44,26 @@ class TestForecast(TestCase):
             h = h + timedelta(hours=1)
 
         assert check_historical_data_present(hour) == True
+
+    def test_forecast_preparation(self):
+        def weather_task(api_token, city_name, start, end):
+            return current_weather_forecast()
+
+        assert len(WeatherForecast.query.all()) == 0
+
+        hour = hour_now()
+
+        prepare_forecast(weather_task, "test_token", "Berlin", hour)
+
+        forecasts = WeatherForecast.query.all()
+
+        for i in range(WEATHER_FORECAST_HOURS_FUTURE):
+            assert forecasts[i].timestamp == hour + timedelta(hours=i)
+
+        assert len(forecasts) in [
+            WEATHER_FORECAST_HOURS_FUTURE,
+            WEATHER_FORECAST_HOURS_FUTURE + 1,
+        ]
 
     def setUp(self):
         db.create_all()
